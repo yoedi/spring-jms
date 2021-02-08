3b7a456394c793c30ae8983c24231e169d1837bd
yoedi123 <yoediarianto@gmail.com>
2021-02-08 20:43:11 +0700

    commit consumer

---
+++ spring-jms-consumer/src/main/java/com/yoedi/springjmsconsumer/SpringJmsConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.yoedi.springjmsconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJmsConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJmsConsumerApplication.class, args);
+	}
+
+}
---
+++ spring-jms-consumer/src/test/java/com/yoedi/springjmsconsumer/SpringJmsConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoedi.springjmsconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJmsConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
